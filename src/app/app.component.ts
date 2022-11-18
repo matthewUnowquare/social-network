1b240be99c34397413ad03cf55f318dc4ba2ff68
matthew seidel <mseidel@misena.edu.co>
2022-11-18 16:07:16 -0500

    add eslint config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector:    'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls:   ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'social-network-angular';
+  mylongvariablename = 'very logn';
 }
